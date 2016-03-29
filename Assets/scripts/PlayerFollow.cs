b69c28a3b3b3d80a4f5706157c034ec49e4f5fe9
Severine Febvet <severinefebvet@gmail.com>
2016-03-29 12:52:36 +0900

    simple script for camera follow

---
+++ Assets/scripts/PlayerFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFollow : MonoBehaviour {
+
+	public	GameObject	player;
+
+	void Start () {
+	
+	}
+
+	//TODO: do it better (get level size and only move when necessary)
+	void Update () {
+		transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+	}
+}
