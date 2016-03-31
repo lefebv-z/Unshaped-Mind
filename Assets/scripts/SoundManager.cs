8bf1db7e8ca01607a9b9825e9ae9188d5724112d
hounak_e <enam.hounakekouassi@epitech.eu>
2016-03-31 16:00:40 +0900

    Adding sounds and sound manager

---
+++ Assets/scripts/SoundManager.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioClip firstStratumMusic;
+    public AudioClip firstStratumBeats;
+
+    public AudioClip secondStratumMusic;
+    public AudioClip secondStratumBeats;
+    
+    public AudioClip logMusic;
+
+    public AudioClip unlockingSound;
+    public AudioClip selectingSound;
+    public AudioClip validationSound;
+    public AudioClip restartSound;
+
+    public AudioSource mainSource; //play main music
+    public AudioSource secondarySource; //play beats of main music
+    public AudioSource noisesSource; // play action sound
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        secondarySource.timeSamples = mainSource.timeSamples; 
+	}
+
+    void PlayLogMenu()
+    {
+        mainSource.Stop();
+        mainSource.loop = true;
+        mainSource.clip = logMusic;
+        mainSource.Play();
+
+        secondarySource.Stop();   
+    }
+
+    void PlayFirstStratum()
+    {
+        mainSource.Stop();
+        mainSource.loop = true;
+        mainSource.clip = firstStratumMusic;
+        mainSource.Play();
+
+        secondarySource.Stop();
+        secondarySource.loop = true;
+        secondarySource.clip = firstStratumBeats;
+        secondarySource.Play();
+    }
+
+    void PlaySecondStratum()
+    {
+        mainSource.Stop();
+        mainSource.loop = true;
+        mainSource.clip = secondStratumMusic;
+        mainSource.Play();
+
+        secondarySource.Stop();
+        secondarySource.loop = true;
+        secondarySource.clip = secondStratumBeats;
+        secondarySource.Play();
+    }
+
+    void PlayUnlocking()
+    {
+        noisesSource.PlayOneShot(unlockingSound);
+    }
+
+    void PlaySelecting()
+    {
+        noisesSource.PlayOneShot(selectingSound);
+    }
+
+    void PlayValidation()
+    {
+        noisesSource.PlayOneShot(validationSound);
+    }
+
+    void PlayRestart()
+    {
+        noisesSource.PlayOneShot(restartSound);
+    }
+
+    void PlayUnlocking(Vector3 pos)
+    {
+        Vector3 oldpos = noisesSource.transform.position;
+        noisesSource.transform.position = pos;
+        noisesSource.PlayOneShot(unlockingSound);
+        noisesSource.transform.position = oldpos;
+    }
+
+    void PlaySelecting(Vector3 pos)
+    {
+        Vector3 oldpos = noisesSource.transform.position;
+        noisesSource.transform.position = pos;
+        noisesSource.PlayOneShot(selectingSound);
+        noisesSource.transform.position = oldpos;
+    }
+
+    void PlayValidation(Vector3 pos)
+    {
+        Vector3 oldpos = noisesSource.transform.position;
+        noisesSource.transform.position = pos;
+        noisesSource.PlayOneShot(validationSound);
+        noisesSource.transform.position = oldpos;
+    }
+
+    void PlayRestart(Vector3 pos)
+    {
+        Vector3 oldpos = noisesSource.transform.position;
+        noisesSource.transform.position = pos;
+        noisesSource.PlayOneShot(restartSound);
+        noisesSource.transform.position = oldpos;
+    }
+}
