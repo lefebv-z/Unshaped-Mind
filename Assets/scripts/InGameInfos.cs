3562bad6d3c084a0fa328b6a1da873544c28fe26
marie.lefebvre@epitech.eu <marie.lefebvre@epitech.eu>
2016-04-18 16:05:33 +0900

    shape restriction feedback (only on level2 for now)

--- Assets/scripts/InGameInfos.cs
+++ Assets/scripts/InGameInfos.cs
@@ -1,18 +1,38 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class InGameInfos : MonoBehaviour {
 
-	//private Shape		playerShape;
+	private Shape		playerShape;
 	private GameManager	gameManager;
 	private Text		remaining;
+	private List<GameObject>	availableShapes;
+	private List<GameObject>	unavailableShapes;
 
 	// Use this for initialization
 	void Start () {
-		//playerShape = GameObject.Find ("PlayerShape").GetComponent<Shape> ();
+		playerShape = GameObject.Find ("PlayerShape").GetComponent<Shape> ();
 		gameManager = GameObject.Find ("GameManager").GetComponent<GameManager> ();
 		remaining = GetComponentInChildren<Text>();
+
+		availableShapes = new List<GameObject> ();
+		unavailableShapes = new List<GameObject> ();
+
+		int i;
+		for (i = 0; i < (int)ShapeType.ShapeTypeCount; i++) {
+			print ("Img_" + ((ShapeType)i).ToString() + "_Available");
+			availableShapes.Add(GameObject.Find("Img_" + ((ShapeType)i).ToString() + "_Available"));
+			unavailableShapes.Add(GameObject.Find("Img_" + ((ShapeType)i).ToString() + "_Unavailable"));
+		}
+
+		i = 0;
+		foreach (bool available in playerShape.shapeAvailable) {
+			availableShapes[i].gameObject.SetActive(available);
+			unavailableShapes[i].gameObject.SetActive(!available);
+			i++;
+		}
 	}
 	
 	// Update is called once per frame
