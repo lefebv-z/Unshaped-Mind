95211b0df002af977f0b7617a4984f0089b0af83
marie.lefebvre@epitech.eu <marie.lefebvre@epitech.eu>
2016-06-05 18:39:20 +0900

    missing script ...sorry ^^'

---
+++ Assets/scripts/ScriptHelper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptHelper : MonoBehaviour {
+
+	public static void IncCursor(ref int cursor, int length) {
+		cursor++;
+		if (cursor == length)
+			cursor = 0;
+	}
+
+	public static void DecCursor(ref int cursor, int length) {
+		cursor--;
+		if (cursor == -1)
+			cursor = length - 1;
+	}
+}
