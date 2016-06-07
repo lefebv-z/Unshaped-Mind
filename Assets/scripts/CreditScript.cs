0a48547372961c51ce8b10cdac9f68247a8b95cc
hounak_e <enam.hounakekouassi@epitech.eu>
2016-06-07 13:20:15 +0200

    Change the printing of credits. More sexy :)

--- Assets/scripts/CreditScript.cs
+++ Assets/scripts/CreditScript.cs
@@ -8,14 +8,19 @@ public class CreditScript : MonoBehaviour {
     private float currTime;
     private string initialText;
     private string otherText;
+
+    public Text pressButton;
+    private string pressButtonText;
+    private string pressButtonOldText;
     private Text mt;
 
 	// Use this for initialization
     void Start()
     {
-        otherText = "Musics\n \"Floating Cities\" \"Thunderbird\" \"Fairytale Waltz\" \"Orion 300XB\" Kevin MacLeod (incompetech.com)\nLicensed under Creative Commons: By Attribution 3.0 License \nhttp://creativecommons.org/licenses/by/3.0/";
+        otherText = "Musics: \"Floating Cities\" \"Thunderbird\" \"Fairytale Waltz\" \"Orion 300XB\" Kevin MacLeod (incompetech.com)\nLicensed under Creative Commons: By Attribution 3.0 License \nhttp://creativecommons.org/licenses/by/3.0/";
         currTime = 0.0f;
         mt = this.GetComponent<Text>();
+        pressButtonText = "";
     }
 	
 	// Update is called once per frame
@@ -24,10 +29,17 @@ public class CreditScript : MonoBehaviour {
         if (currTime >= changeTime)
         {
             currTime = 0.0f;
+            pressButtonOldText = pressButton.text;
+            pressButton.text = pressButtonText;
+            pressButtonText = pressButtonOldText;
+        }
+        currTime += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
             initialText = mt.text;
             mt.text = otherText;
             otherText = initialText;
         }
-        currTime += Time.deltaTime;
     }
 }
